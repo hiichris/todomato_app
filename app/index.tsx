77fcad6bbf3d8af432977c3020927365332d5a46
Y <x@tofr.me>
2024-06-13 14:45:03 -0700

    Explore expo notification and eas build

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, Platform } from 'react-native';
+
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>hello</Text>
+
     </View>
   );
 }
